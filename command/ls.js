40c041c4cd128408d37d56b0ddfbb4232554e7ac
dugzh <dugzh@chinatelecom.cn>
2018-05-14 09:30:46 +0800

    format ls logs

--- command/ls.js
+++ command/ls.js
@@ -3,10 +3,21 @@
 const chalk = require('chalk')
 const config = require('./config')
 
+function _fixStrLen(str, len) {
+  if (str.length >= len) {
+    return str.substring(0, (len - 2)) + '… '
+  }
+  let fitLen = len - str.length;
+  for (let i = 0; i < fitLen; i++) {
+    str += ' '
+  }
+  return str;
+}
+
 module.exports = () => {
   console.log(chalk.magenta('Supports Templates:'))
   Object.keys(config.templates).forEach(item => {
-    console.log('%s \t %s', chalk.green(item), config.templates[item].desc)
+    console.log('%s %s', chalk.green(_fixStrLen(item, 20)), config.templates[item].desc)
   })
   process.exit()
 }
